056e3cc3b84cffd919927b7d865616dedd24c81f
Aziz Lokhandwala <36584897+azizl@users.noreply.github.com>
2018-03-06 16:03:36 +0530

    Create ultrasonic.ino

---
+++ ultrasonic.ino
@@ -0,0 +1,32 @@
+const int pin = 7;
+
+void setup(){
+   Serial.begin(9600);
+}
+void loop(){
+  long duration, inches, cm;
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pin, LOW);
+  pinMode(pin, INPUT);
+  duration = pulseIn(pin, HIGH);
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+   delay(100);
+}
+
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
